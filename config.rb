50d15b94dc631ae18aeea1f2c3370d1ef7604ac8
Dunya Kirkali <dunyakirkali@gmail.com>
2017-02-18 16:24:48 +0100

    Correct tag manager

--- config.rb
+++ config.rb
@@ -11,10 +11,5 @@ configure :build do
   activate :minify_css
   activate :minify_javascript
   activate :asset_hash
-    activate :gzip
-end
-
-activate :google_tag_manager do |gtm|
-  gtm.container_id = 'GTM-W7ZH2LP'
-  gtm.development = true
+  activate :gzip
 end
